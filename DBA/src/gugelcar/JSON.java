6e6404d7057b0d7bcba8da17751d263356c0d312
danidiaz1 <danieldiazpareja1@gmail.com>
2017-10-17 11:10:41 +0200

    Primer commit

---
+++ DBA/src/gugelcar/JSON.java
@@ -0,0 +1,19 @@
+/* Crear librería JSON con el siguiente JAR para obtener la funcionalidad de la
+biblioteca org.json: https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/json-simple/json-simple-1.1.1.jar
+*/
+
+package gugelcar;
+import org.json.simple.parser.JSONParser;
+/**
+ * @brief Clase encargada de obtener los atributos necesarios de un String
+ * codificado en formato JSON.
+ * @author Daniel Díaz Pareja
+ */
+public class JSON {
+    private String cadena = "";
+    private JSONParser parser;
+    
+    public JSON(String cadena){
+        
+    }
+}
