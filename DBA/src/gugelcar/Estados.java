8eb6b80b88e0faba555d7632e4978064595a9ce0
jorge <jorge@jorge-pc>
2017-10-18 14:25:27 +0200

    Creo enum Estados. Añado diagrama de clases GugelCar

---
+++ DBA/src/gugelcar/Estados.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gugelcar;
+
+/**
+ *
+ * @author jorge
+ */
+public enum Estados {
+OK, CRASHED, BAD_COMMAND, BAD_PROTOCOL, BAD_KEY
+}
--- DBA/src/gugelcar/GugelCar.java
+++ DBA/src/gugelcar/GugelCar.java
@@ -5,12 +5,55 @@
  */
 package gugelcar;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author dani
  */
-public class GugelCar {
 
-   
-    
+public class GugelCar {
+private ArrayList<ArrayList<int>> map;
+private String login;
+private String password;
+private String clave_acceso;
+private int numero_mapa;
+private int bateria;
+private int pos_x;
+private int pos_y;
+private ArrayList<float> lectura_escaner;
+private Estados estado_actual;
+
+public GugelCar(String aid){
+
+}
+
+public void login(){
+
+}
+
+public void logout(){
+
+}
+
+public void mover(String direccion){
+
+}
+
+public void refuel(){
+
+}
+
+public void actualizarMapa(){
+
+}
+
+public void decidir(){
+
+}
+
+public void estoyEnObjetivo(){
+
+}
+
 }
