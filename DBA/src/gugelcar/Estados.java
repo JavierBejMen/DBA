2ffbe1a905a713957dba7405841928f50861184e
DaniDiazP <diaz_alr@hotmail.com>
2017-10-21 21:00:05 +0200

    Añado enum "Movimientos.java". Termino parser JSON, clase "JSON.java", con pruebas realizadas en main.

--- DBA/src/gugelcar/Estados.java
+++ DBA/src/gugelcar/Estados.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gugelcar;
 
 /**
- *
- * @author jorge
+ * @brief Posibles estados del agente
+ * @author Jorge 
  */
 public enum Estados {
-OK, CRASHED, BAD_COMMAND, BAD_PROTOCOL, BAD_KEY
+    OK, CRASHED, BAD_COMMAND, BAD_PROTOCOL, BAD_KEY;
 }
