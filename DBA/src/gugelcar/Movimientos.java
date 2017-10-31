3b9c4c1bc98a78aa84985f961324c5203190e505
JavierBejMen <j.bejar.mendez@gmail.com>
2017-10-31 10:17:04 +0100

    Añadido Javier a autores

--- DBA/src/gugelcar/Movimientos.java
+++ DBA/src/gugelcar/Movimientos.java
@@ -2,7 +2,7 @@ package gugelcar;
 
 /**
  * @brief Posibles movimientos del agente
- * @author Daniel Díaz Pareja
+ * @author Daniel Díaz Pareja, Javier Bejar Mendez
  */
 public enum Movimientos {
     moveN, moveS, moveE, moveW, moveNE, moveNW, moveSE, moveSW,refuel;
