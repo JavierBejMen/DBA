ead46b871d215c3ad8226b124b32709df1314917
JavierBejMen <j.bejar.mendez@gmail.com>
2017-12-12 11:08:47 +0100

    Implementado Registrarse() en clase AgenteVehiculo, falta depurar #12

--- DBAP3/src/gugelcar/AgenteVehiculo.java
+++ DBAP3/src/gugelcar/AgenteVehiculo.java
@@ -35,6 +35,10 @@ public class AgenteVehiculo extends SingleAgent{
     private boolean estoy_en_objetivo;
     private Posicion serverPos; //No tengo claro si tenemos distinción entre internalPos y serverPos, el martes lo resolvemos.
     
+    //Comunicacion
+    JSON jsonobj;
+    
+    
     /**
      * @brief Constructor
      * @author Javier Bejar Mendez
@@ -49,7 +53,35 @@ public class AgenteVehiculo extends SingleAgent{
      * @brief Realiza el request "checkin" para obtener la información del agente
      * @author Javier Bejar Mendez
      */
-    public void registrarse(){}
+    public void registrarse(){
+    ACLMessage outbox = null;
+    
+    outbox = new ACLMessage();
+    outbox.setSender(aid);
+    outbox.setReceiver(controlador_id);
+    outbox.setContent(jsonobj.encodeCheckin());
+    outbox.setConversationId(conversation_id);
+    outbox.setPerformative(ACLMessage.REQUEST);
+    
+    ACLMessage inbox = null;
+    
+    this.send(outbox);
+    try{
+    inbox = receiveACLMessage();
+    }catch(Exception ex){
+        System.out.println("Error al recivir respuesta del chek-in del controlador"+ex.getMessage());
+    }
+    
+    if(inbox.getPerformative() == jsonobj.decodeErrorControlador(inbox.getPerformative())){
+        System.out.println("Error: el controlador no entiende el check-in");
+    }else if(inbox.getPerformativeInt() == ACLMessage.INFORM){
+        ArrayList<Object> capabilities = jsonobj.decodeCapabilities(inbox.getContent());
+        this.fuelrate = (int)capabilities.get(0);
+        this.range = (int)capabilities.get(1);
+        this.fly = (boolean)capabilities.get(2);
+    }
+    
+    }
     
     /**
      * @brief Comportamiento del agente
